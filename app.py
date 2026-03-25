7ed053d3005258bb0693b914e6cda6ed6e2c4971
yasrizalhakim <yasrizalhakim@gmail.com>
2026-03-25 12:06:11 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask (__name__)
+
+@app.route("/")
+def home():
+    return "hello from my container!"
+
+@app.route("/health")
+def health():
+    return "App is running fine!"
+
+@app.route("/well")
+def well():
+    return "App API is ONLINE!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
