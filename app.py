b6f2c8ca921f2e395b8017c9f5a761b1ce3fc4a2
yasrizalhakim <yasrizalhakim@gmail.com>
2026-03-25 15:06:08 +0800

    compose stack update

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask
+from redis import Redis
 
 app = Flask (__name__)
+redis = Redis(host='redis', port=6379)
 
 @app.route("/")
 def home():
-    return "hello from my CI/CD pipeline!"
+    count = redis.incr('visits')
+    return f"This page has been visited {count} times."
 
 @app.route("/health")
 def health():
     return "App is running fine!"
-
-@app.route("/well")
-def well():
-    return "App API is ONLINE!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
