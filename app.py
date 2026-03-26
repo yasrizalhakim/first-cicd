a729fc705283e4d490551bfff9ae1f83002becf0
yasrizalhakim <yasrizalhakim@gmail.com>
2026-03-26 11:31:30 +0800

    personalized app

--- app.py
+++ app.py
@@ -12,7 +12,15 @@ def home():
 
 @app.route("/health")
 def health():
-    return "App is running fine!"
+    return "App Health is running OK!"
+
+@app.route("/1")
+def route1():
+    return "This is route 1"
+
+@app.route("/2")
+def route2():
+    return "This is route 2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
